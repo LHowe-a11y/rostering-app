6dec8f6981cc733a3bbf56a32772435eebc14136
LH <liam.howe2@education.nsw.gov.au>
2025-05-18 16:55:31 +1000

    Made functional rudimentary login system. Only works using single admin test account, setting up database and sign up page is on to do list. Security features will be implemented alongside this; session management is rudimentary and input sanitisation is not yet implemented.

--- app.py
+++ app.py
@@ -1,12 +1,38 @@
-from flask import Flask
+from flask import Flask, request, session, redirect, flash, render_template
+import os
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def home():
+    if session.get("logged_in"):
+        name = session.get("username")
+        return "Logged in, %s" % name
+    else:
+        return redirect("/login")
+
+
+@app.route("/login", methods=["POST", "GET"])  # type: ignore
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        # TODO make a real sign up page and real logins with input sanitisation
+        if username == "admin" and password == "123456":
+            session["logged_in"] = True
+            session["username"] = username
+            # TODO implement secure session management with secret key? permanent sessins and session timeouts
+        else:
+            flash("Incorrect username or password.", "error")
+        return redirect("/")
+    elif request.method == "GET":
+        if not session.get("logged_in"):
+            return render_template("login.html")
+        else:
+            return redirect("/")
 
 
 if __name__ == "__main__":
-    app.run()
+    app.secret_key = os.urandom(24)
+    app.run(debug=True)
