8404a144425159bf115d10716f2061cb0c0c6b32
LH <liam.howe2@education.nsw.gov.au>
2025-05-21 11:59:57 +1000

    Created accounts manager class for account creation and user authentication

--- handler.py
+++ handler.py
@@ -1,3 +1,48 @@
+import sqlite3 as sql
+from public.scripts.tools import hash, check_hash
+
+
 class AccountsManager:
     def __init__(self) -> None:
         pass
+
+    def create_account(self, username: str, password: str) -> None:
+        hashed_password = hash(password)
+        db = sql.connect(".database/accounts.db")
+        cursor = db.cursor()
+        cursor.execute(
+            "INSERT INTO accounts (username,password) VALUES (?,?)",
+            (username, hashed_password),
+        )
+        db.commit()
+        db.close()
+        return
+    
+    def verify_login(self, username: str, password: str) -> tuple:
+        db = sql.connect(".database/accounts.db")
+        cursor = db.cursor()
+        res = cursor.execute(
+            f"SELECT * FROM accounts WHERE username = {username}"
+        )
+        if len(cursor.fetchall()) > 1:
+            abc = res.fetchall()
+            db.close()
+            raise Exception("More than one account with username", abc)
+        elif res.fetchone() is None:
+            db.close()
+            return (False, 1)
+        res = cursor.execute(
+            f"SELECT password FROM accounts WHERE username = {username}"
+        )
+        fetch = res.fetchone()
+        password_hash = fetch[0]
+        if not check_hash(password, password_hash):
+            db.close()
+            return (False, 2)
+        res = cursor.execute(
+            f"SELECT id FROM accounts WHERE username = {username}"
+        )
+        user_id = res.fetchone()[0]
+        db.close()
+        return (True, user_id)
+
