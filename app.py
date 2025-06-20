c90de98a210b617c61ef0e2748a27aaa7bc48fd7
LH <liam.howe2@education.nsw.gov.au>
2025-06-20 11:41:22 +1000

    Enforced input sanitisation and validation for adding dentist, added comments to code logic in app file

--- app.py
+++ app.py
@@ -4,10 +4,13 @@ from public.scripts.tools import sanitise, Roster
 from handler import DatabaseManager
 import json
 
+# Initialise flask application
 app = Flask(__name__)
 
+# Initialise database manager
 manager = DatabaseManager()
 
+# Test data
 example_dentists = [
     {
         "day": "monday",
@@ -88,14 +91,13 @@ example_employees = [
     },
 ]
 
-example_dentists_json = json.dumps(example_dentists)
-new_dentists = json.loads(example_dentists_json)
-
 
 @app.route("/")
 def home():
+    # Redirect either to login or to roster tool immediately
     if session.get("logged_in"):
         id = session.get("id")
+        # Redirect to login and correct false data (security architecture)
         if id is None or id == 0:
             session["logged_in"] = False
             flash("You were logged in to an invalid account.", "error")
@@ -107,17 +109,22 @@ def home():
 
 @app.route("/login", methods=["POST", "GET"])  # type: ignore
 def login():
+    # If attempt at login
     if request.method == "POST":
+        # Get login data and sanitise (escapes bad inputs)
         username = sanitise(request.form["username"])
         password = sanitise(request.form["password"])
+        # Verify data
         if manager.verify_login(username, password):
             session["logged_in"] = True
             session["id"] = manager.user_id(username, password)
             # TODO implement secure session management with permanent sessions and session timeouts
         else:
             flash("Incorrect username or password.", "error")
+        # Redirect to home directory
         return redirect("/")
     elif request.method == "GET":
+        # Display login page (or redirect if already logged in)
         if not session.get("logged_in"):
             return render_template("login.html")
         else:
@@ -126,18 +133,23 @@ def login():
 
 @app.route("/signup", methods=["POST", "GET"])  # type: ignore
 def sign_up():
+    # Render login template
     if request.method == "GET":
         return render_template("signup.html")
     elif request.method == "POST":
+        # Get account data and sanitise
         username = sanitise(request.form["username"])
         password = sanitise(request.form["password"])
         password_two = sanitise(request.form["confirm_password"])
+        # Verify unique username
         if not manager.username_is_unique(username):
             flash("Username is taken.", "error")
             return render_template("signup.html")
+        # Verify same password as confirmed password (to avoid accidental typos)
         if not password == password_two:
             flash("Passwords do not match.", "error")
             return render_template("signup.html")
+        # Add account to database
         manager.create_account(username, password)
         flash("Account successfully created, feel free to log in.", "message")
         return redirect("/login")
@@ -153,34 +165,82 @@ def tool():
     # if request.method == "POST":
     # TODO get the actual inputs from the web page
     if request.method == "POST":
+        # If statement determines user action/request
         if request.form["submit"] == "Add dentist":
+            # Fetch data
             user_id = session.get("id")
             json_old_dentists = manager.fetch_in_progress_dentists(user_id)  # type: ignore
+            # Prevent error from non-existent database entry
             if json_old_dentists is None:
                 dental_shifts = []
             else:
                 dental_shifts = json.loads(json_old_dentists)
+            # Format data from inputs into intended format and sanitise and validate it
+            day = sanitise(request.form["day"])
+            if (
+                day != "monday"
+                and day != "tuesday"
+                and day != "wednesday"
+                and day != "thursday"
+                and day != "friday"
+                and day != "saturday"
+            ):
+                flash("Please give day in lowercase text form", "error")
+                # TODO implement flash messages into roster page template
+                return redirect("/login")
+            start = sanitise(request.form["start"])
+            try:
+                test = float(start)  # noqa: F841
+            except ValueError:
+                flash(
+                    "Please enter start and end times in hours, decimal, e.g. 9am-5:45pm is 9.0-17.75"
+                )
+                return redirect("/login")
+            except TypeError:
+                flash(
+                    "Please enter start and end times in hours, decimal, e.g. 9am-5:45pm is 9.0-17.75"
+                )
+                return redirect("/login")
+            end = sanitise(request.form["start"])
+            try:
+                test = float(end)  # noqa: F841
+            except ValueError:
+                flash(
+                    "Please enter start and end times in hours, decimal, e.g. 9am-5:45pm is 9.0-17.75"
+                )
+                return redirect("/login")
+            except TypeError:
+                flash(
+                    "Please enter start and end times in hours, decimal, e.g. 9am-5:45pm is 9.0-17.75"
+                )
+                return redirect("/login")
             new_shift = {
-                "day": request.form["day"],
-                "start": request.form["start"],
-                "end": request.form["end"],
+                "day": day,
+                "start": float(start),
+                "end": float(end),
                 "id": request.form["id"],
             }
             dental_shifts.append(new_shift)
             json_dentists = json.dumps(dental_shifts)
+            # Create new database entry to update if none exists
             if json_old_dentists is None:
                 manager.new_in_progress(user_id)  # type: ignore
+            # Update in progress database
             manager.update_in_progress_dentists(user_id, json_dentists)  # type: ignore
         elif request.form["submit"] == "Delete last dentist":
+            # fetch data
             user_id = session.get("id")
             json_old_dentists = manager.fetch_in_progress_dentists(user_id)  # type: ignore
+            # Verify there is something to delete
             if json_old_dentists is not None:
                 dental_shifts = json.loads(json_old_dentists)
                 if dental_shifts != []:
+                    # delete
                     dental_shifts.pop()
                     json_dentists = json.dumps(dental_shifts)
                     manager.update_in_progress_dentists(user_id, json_dentists)  # type: ignore
 
+    # Render template using test data
     blank_roster = Roster(example_dentists, example_employees)
     # test_table = blank_roster.display_roster([])
     blank_roster.create_roster()
@@ -189,5 +249,7 @@ def tool():
 
 
 if __name__ == "__main__":
+    # Secret key for encryption
     app.secret_key = os.urandom(24)
+    # Debug mode, run web server!
     app.run(debug=True)
